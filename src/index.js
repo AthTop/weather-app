45d556ca12e9e59faeb2b05c1a32352dc00eab45
Sakis T. <106918030+AthTop@users.noreply.github.com>
2024-10-30 18:24:57 +0200

    Add some error handling. Add loading animation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css'
 
+const app = document.querySelector('#app')
 const API_KEY = 'SG64JKKZXH2K8S8F9AB6G8JJ6'
 let location = ''
 const url =
@@ -7,12 +8,18 @@ const url =
 
 // Handle API access
 async function getWeatherData(location) {
-    const response = await fetch(
-        url + location + '?unitGroup=metric' + '&key=' + API_KEY
-    )
-    const data = await response.json()
-    displayWeather(filterData(data))
+    try {
+        loading()
+        const response = await fetch(
+            url + location + '?unitGroup=metric' + '&key=' + API_KEY
+        )
+        const data = await response.json()
+        displayWeather(filterData(data))
+    } catch (error) {
+        console.error(error)
+    }
 }
+
 // Filter json data
 function filterData(data) {
     return {
@@ -37,13 +44,24 @@ searchBtn.addEventListener('click', (e) => {
 // Function to display weather data
 
 function displayWeather(data) {
-    const div = document.getElementById("weather-card")
-    div.innerHTML = ""
+    removeLoading()
+    const div = document.getElementById('weather-card')
+    div.innerHTML = ''
     const content = `<h2>Location: ${data.address}</h2>
     <p>Time: ${data.currentTime}</p>
     <p>Conditions: ${data.conditions}</p>
     <p>Temperature: ${data.temp}</p>`
     div.innerHTML = content
-    const app = document.querySelector('#app')
     app.append(div)
+}
+
+function loading() {
+    const div = document.createElement("div")
+    div.className = 'loader'
+    app.append(div)
+}
+
+function removeLoading() {
+    const div = document.querySelector("div + .loader")
+    div.remove()
 }
